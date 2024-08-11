9e9bb65cadcc51d18c08b945d79bff7661a59471
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-08-12 00:34:07 +0200

    some changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import css from '../Profile/Profile.module.css';
 export default function Profile({ name, tag, image, location, stats }) {
   return (
     <div className={css.container}>
-      <div className={css.containerProfile}>
+      <div className={css.containerUserInfo}>
         <img className={css.img} src={image} alt="User avatar" />
         <p className={css.name}>{name}</p>
         <p className={css.tag}>@{tag}</p>
