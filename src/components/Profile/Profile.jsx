7002304a814e71bb8efcd37c0918d953404ef4ae
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-08-12 19:13:24 +0200

    add some changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,23 +3,26 @@ export default function Profile({ name, tag, image, location, stats }) {
   return (
     <div className={css.container}>
       <div className={css.containerUserInfo}>
-        <img className={css.img} src={image} alt="User avatar" />
+        <img className={css.img} 
+        src={image} 
+        alt="User avatar" />
         <p className={css.name}>{name}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
+
       <ul className={css.list}>
         <li className={css.stats}>
           <p>Followers</p>
-          <p>{stats.followers}</p>
+          <p className={css.statsNumbers}>{stats.followers}</p>
         </li>
         <li className={css.stats}>
           <p>Views</p>
-          <p>{stats.views}</p>
+          <p className={css.statsNumbers}>{stats.views}</p>
         </li>
         <li className={css.stats}>
           <p>Likes</p>
-          <p>{stats.likes}</p>
+          <p className={css.statsNumbers}>{stats.likes}</p>
         </li>
       </ul>
     </div>
