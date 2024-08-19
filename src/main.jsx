cf94648aff6f63ad0fd6cd26638a318e5fdbacc5
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-08-19 02:55:26 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import "modern-normalize";
 import './index.css';
+import 'modern-normalize';
 import App from './components/App/App';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
